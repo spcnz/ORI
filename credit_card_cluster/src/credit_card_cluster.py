439b46ccb9b15e757fbd23c76b696cf44a457c1e
specnazm <laketic.milena98@gmail.com>
2020-07-03 02:22:58 +0200

    Visualization of clasters using PCA

--- credit_card_cluster/src/credit_card_cluster.py
+++ credit_card_cluster/src/credit_card_cluster.py
@@ -1,8 +1,53 @@
 from preprocess_data import prepare_data
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def find_optimal_K(data):
+    #Elbow method
+    distortions = []
+    for i in range(1, 11):
+        km = KMeans(
+            n_clusters=i, init='random',
+            n_init=10, max_iter=300,
+            tol=1e-04, random_state=0
+        )
+        km.fit(data)
+        distortions.append(km.inertia_)
+
+    # plot
+    plt.plot(range(1, 11), distortions, marker='o')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Distortion')
+    plt.show()
+
+
 
 if __name__ == '__main__':
     file_path = "../data/credit_card_data.csv"
     data = prepare_data(file_path)
+
+    sns.catplot(kind="box", data=data)
+    plt.show()
+
+    #transform data so it can be represented to 2D
+    pca = PCA(n_components=0.95)
+    pca.fit(data)
+    principalComponents = pca.transform(data)
+
+    find_optimal_K(principalComponents)
+
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(principalComponents)
+    labels = kmeans.predict(principalComponents)
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel("PC 0")
+    ax.set_ylabel("PC 1")
+    plt.scatter(principalComponents[:, 0], principalComponents[:, 1],
+                c=labels.astype(float))
+    plt.show()
 
 
 
--- credit_card_cluster/src/preprocess_data.py
+++ credit_card_cluster/src/preprocess_data.py
@@ -2,30 +2,32 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
 import numpy as np
 
 def scale_values(data):
     # data = data.apply(lambda x: (x - x.mean())/ x.std())
     scaler = StandardScaler()
     scaled = scaler.fit_transform(data)
-    data = pd.DataFrame(scaled, columns=data.columns)
+    data = pd.DataFrame(scaled, index=data.index, columns=data.columns)
+
     return data
 
-
 def treat_outliers(data):
-    data.plot(kind='box')
+    #data.plot(kind='box')
 
-    for column in data.columns:
-        upper_bound = data[column].quantile(0.95)
-        lower_bound = data[column].quantile(0.05)
-        data[column] = np.where(data[column] > upper_bound, upper_bound, data[column])
-        data[column] = np.where(data[column] < lower_bound, lower_bound, data[column])
+    # for column in data.columns:
+    #     upper_bound = data[column].quantile(0.95)
+    #     lower_bound = data[column].quantile(0.05)
+    #     data[column] = np.where(data[column] > upper_bound, upper_bound, data[column])
+    #     data[column] = np.where(data[column] < lower_bound, lower_bound, data[column])
 
     #Q1 = data.quantile(0.25)
     # Q3 = data.quantile(0.75)
     # IQR = Q3 - Q1
-    sns.catplot(x="BALANCE", kind="box", data=data)
-    data.plot(kind='box')
+    # sns.catplot(x="BALANCE", kind="box", data=data)
+    # data.plot(kind='box')
     #
     # print(data.shape)
     # print(data < (Q1 - 1.5 * IQR)) | (data > (Q3 + 1.5 * IQR))
@@ -34,10 +36,9 @@ def treat_outliers(data):
     # print(data.shape)
 
     # perform quantile clipping in order to treat outliers
-    # data = data.applymap(lambda x: np.log(x + 1))
+    data = data.applymap(lambda x: np.log(x + 1))
     # sns.catplot(x="BALANCE", kind="box", data=data)
-    # plt.show()
-    plt.show()
+
     return data
 
 def remove_null_values(data):
@@ -47,17 +48,13 @@ def remove_null_values(data):
 
     return data
 
-
 def prepare_data(path):
     data = pd.read_csv(path)
-    data = remove_null_values(data)
-    # Removes non utile columns (CUSTID, TENURE)
+    # Removes non utile columns
     data = data.drop(['CUST_ID'], axis=1)
+    data = remove_null_values(data)
     data = treat_outliers(data)
     data = scale_values(data)
-
-    print(data)
-    #ODREDITI KORELACIJU I IZBACIITI NEPOTREBNE KOLONE
 
     return data
 
