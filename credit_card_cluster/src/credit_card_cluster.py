a51b8bc223b1ff46a27230657b5895eb5f92d0f7
specnazm <laketic.milena98@gmail.com>
2020-07-04 02:05:37 +0200

    Customized legend on graph

--- credit_card_cluster/src/credit_card_cluster.py
+++ credit_card_cluster/src/credit_card_cluster.py
@@ -3,10 +3,10 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
 import numpy as np
-import matplotlib.patches as mpatches
 from decision_tree import cluster_report
+
+colors = ['green','orange','pink','dodgerblue','red']
 
 def find_optimal_K(data):
     #Elbow method
@@ -30,6 +30,7 @@ def show_eda_information(data):
     # Visualize box plot after data preprocess(there shouldn't be outliers)
     chart = sns.catplot(kind="box", data=data)
     chart.set_xticklabels(rotation=45, horizontalalignment='right')
+    plt.show()
 
     #Show correlation between data variables
     cut_data = data[["PURCHASES", "BALANCE", "PAYMENTS", "PURCHASES_FREQUENCY","CASH_ADVANCE"]]
@@ -42,10 +43,11 @@ if __name__ == '__main__':
     data = prepare_data(file_path)
     show_eda_information(data)
 
-    #Transform data to lower dimensions so it can be represented to 2D
-    pca = PCA(n_components=0.95)
+    #Transform data to lower dimensions so it can be represented to 2D, choose number of components so that they explain 90% of the variance
+    pca = PCA(n_components=0.9)
     pca.fit(data)
     principalComponents = pca.transform(data)
+    #print(len(principalComponents[0])) There are 8 dimension now for each row
 
     find_optimal_K(principalComponents)
 
@@ -63,44 +65,12 @@ if __name__ == '__main__':
         legendLabels.append('Cluster ' + str(i) + ' : ' + cluster_rules[i])
 
     fig, ax = plt.subplots()
-    ax.set_title("K-means Clustering with 2 dimensions.")
+    ax.set_title("K-means Clustering with 2 dimensions'")
     ax.set_xlabel("PC 0")
     ax.set_ylabel("PC 1")
-
-
-
-    g = plt.scatter(principalComponents[:, 0], principalComponents[:, 1],
-                c=labels.astype(float), label=legendLabels[0])  # visualize clustering according to first two prinicple components
-
-
-
-    ax.legend()
-    #
-    # plt.title('K-means Clustering with 2 dimensions')
-    # reduced_data = pd.DataFrame()
-    # reduced_data['PC 0'] = principalComponents[:, 0]
-    # reduced_data['PC 1'] = principalComponents[:, 1]
-    # reduced_data['cluster'] = labels
-    # g = sns.scatterplot(x='PC 0', y='PC 1', hue='cluster', data=reduced_data, edgecolor='k')
-    # leg = g.legend_
-    # # g.fig.suptitle("K-means Clustering with 2 dimensions")
-    # leg.get_texts()[0].set_text('Clusters')
-    # # for t, l in zip(leg.texts, legend): t.set_text(l)
-    # print(legendLabels)
-    # for i, label in enumerate(legendLabels):
-    #     # i+1 because i=0 is the title, and i starts at 0
-    #     print( i + 1)
-    #     # print(leg.get_texts()[i + 1])
-    #     # print(i + 1)
-    #     # leg.get_texts()[i + 1].set_text("aaa")
-    #
-    #     # # leg.set_bbox_to_anchor([0.7, 1.3])
-    # #
-    # # leg._loc = 2
-
-
-
+    # visualize clustering according to first two prinicple components
+    scatter = plt.scatter(principalComponents[:, 0], principalComponents[:, 1], c=labels.astype(float))
+    legend = ax.legend(scatter.legend_elements()[0], legendLabels, loc="lower left", title="Clusters")
+    legend.set_bbox_to_anchor([-0.1, -0.4])
+    ax.add_artist(legend)
     plt.show()
-
-
-
